def38696e52a0dbb10ee30eecdc10aa7d214eaaa
maling6154 <maling6154@users.noreply.github.com>
2017-01-28 21:36:43 -0500

    upload file

---
+++ culture.py
@@ -0,0 +1,14 @@
+# construct original seed words based on six culture dimensions, for every dimension, only choose words with
+# positive loadings greater than 0.4
+
+original_seed_words = {"adaptability":
+["being_innovative", "risk_taking", "being_willing_to_experiment", "fast_moving",
+"being_quick_to_take_advantage_of_opportunities", "not_being_constrained_by_many_rules", "adaptability"],
+"integrity":
+["having_integrity", "having_high_ethical_standards", "being_honest", "respecting_individuals",
+"being_fair"],
+"collaborative":["working_in_collaboration_with_others", "being_team_oriented", "cooperative", "being_supportive",
+"avoiding_conflict"],
+"results_oriented":["being_results_oriented", "having_high_expectations_for_performance", "achievement_oriented"],
+"customer_oriented":["being_customer_oriented", "listening_to_customers", "being_market_driven"],
+"detail_oriented":["paying_attention_to_detail", "emphasizing_quality", "being_precise"]}
